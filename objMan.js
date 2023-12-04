b7c9038240bfa86a4537ebb94d42ae1f1ef75c0b
Domi <Domi@PC2013>
2023-12-04 18:24:09 +0100

    loadTypes

--- objMan.js
+++ objMan.js
@@ -1,2 +1,26 @@
-/*jslint beta*/
-/*global*/
+/*jslint beta, eval*/
+/*global
+    libByName
+*/
+
+const objMan = (function () {
+    const types = loadTypes();
+
+    function loadTypes() {
+        const objTypes = {};
+        const typeEntries = libByName("ObjType").entries();
+        typeEntries.forEach(loadObjectType);
+
+        function loadObjectType(typeEntry) {
+            const code = typeEntry.field("Code");
+            const name = typeEntry.field("Name");
+            types[name] = eval(code);
+        }
+
+        return objTypes;
+    }
+
+    return Object.freeze({
+        types
+    });
+}());
