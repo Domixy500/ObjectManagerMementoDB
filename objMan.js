a4ca70323d1cde9011c39d08d063ffb09a638077
Domi <Domi@PC2013>
2023-12-05 19:18:27 +0100

    Create Types
    Obj
    ObjType

--- objMan.js
+++ objMan.js
@@ -6,6 +6,11 @@
 const objMan = (function () {
     const types = loadTypes();
 
+    function createEntry(libName) {
+        const entryLib = libByName(libName);
+        return entryLib.create({});
+    }
+
     function id() {
         const now = Date.now();
 
@@ -21,7 +26,124 @@ const objMan = (function () {
     }
 
     function init() {
-        message("Init");
+        if (libByName("ObjType").entries().length === 0) {
+            createObjAndObjType();
+        }
+
+        function createObjAndObjType() {
+            const obj = createEntry("ObjType");
+            const objType = createEntry("ObjType");
+            obj.set("Name", "Obj");
+            obj.set("DisplayName", "Obj");
+            obj.set("Id", id());
+            obj.set(
+                "Code",
+                `/*jslint beta*/
+/*global*/
+
+(function () {
+    return function (e) {
+        const trigger = {
+            "afterCreate": afterCreate,
+            "afterDelete": afterDelete,
+            "afterSave": afterSave
+        };
+
+        function afterCreate() {
+            name(name());
+        }
+
+        function afterDelete() {
+            name(name());
+        }
+
+        function afterSave() {
+            name(name());
+        }
+
+        function name(newVal) {
+            if (newVal !== undefined) {
+                e.set("Name", newVal);
+            }
+            return e.field("Name");
+        }
+
+        function show() {
+            if (e.show) {
+                e.show();
+            }
+        }
+
+        return Object.freeze({
+            "name": name,
+            "show": show,
+            "trigger": trigger
+        });
+    };
+}());
+`
+            );
+            obj.link("Type", objType);
+            obj.link("HasTypes", obj);
+            obj.link("HasTypes", objType);
+            obj.link("CreateTypes", obj);
+
+            objType.set("Name", "ObjType");
+            objType.set("DisplayName", "ObjType");
+            objType.set("Id", id());
+            objType.set(
+                "Code",
+                `/*jslint beta*/
+/*global*/
+
+(function () {
+    return function (e) {
+        const trigger = {
+            "afterCreate": afterCreate,
+            "afterDelete": afterDelete,
+            "afterSave": afterSave
+        };
+
+        function afterCreate() {
+            name(name());
+        }
+
+        function afterDelete() {
+            name(name());
+        }
+
+        function afterSave() {
+            name(name());
+        }
+
+        function name(newVal) {
+            if (newVal !== undefined) {
+                e.set("Name", newVal);
+            }
+            return e.field("Name");
+        }
+
+        function show() {
+            if (e.show) {
+                e.show();
+            }
+        }
+
+        return Object.freeze({
+            "name": name,
+            "show": show,
+            "trigger": trigger
+        });
+    };
+}());
+`
+            );
+            objType.link("Type", objType);
+            objType.link("HasTypes", obj);
+            objType.link("HasTypes", objType);
+            objType.link("CreateTypes", obj);
+            objType.link("CreateTypes", objType);
+        }
     }
 
     function loadTypes() {
